71dafefeaa0157d65fe36d8cbf72209d5e24fdc3
Markus Klinik <markus.klinik@gmx.de>
2012-02-11 23:06:37 +0100

    gendered role names (King/Queen)

--- Character.hs
+++ Character.hs
@@ -1,5 +1,7 @@
 module Character where
 
+import Data.List
+
 import Describable
 import Item
 
@@ -7,18 +9,36 @@ data Race = Human | Elven | Dwarf
   deriving (Eq, Show, Enum, Bounded)
 
 data Role = Fool
-          | Wizard 
           | Thief
           | Servant
-          | Fisherman
+          | Hunter
           | Peasant
           | Blacksmith
           | Merchant
           | Priest
           | Warrior
+          | Knight
+          | Wizard
           | Prince
           | King
   deriving (Eq, Show, Enum, Bounded)
+
+genderedRoles =
+  --              Male           Female
+  [ (Fool,       ("Fool",        "Fool"))
+  , (Thief,      ("Thief",       "Thief"))
+  , (Servant,    ("Servant",     "Servant"))
+  , (Hunter,     ("Hunter",      "Hunter"))
+  , (Peasant,    ("Peasant",     "Peasant"))
+  , (Blacksmith, ("Blacksmith",  "Blacksmith"))
+  , (Merchant,   ("Merchant",    "Merchant"))
+  , (Priest,     ("Priest",      "Priestess"))
+  , (Warrior,    ("Warrior",     "Warrior"))
+  , (Knight,     ("Knight",      "Knight"))
+  , (Wizard,     ("Wizard",      "Wizard") )
+  , (Prince,     ("Prince",      "Princess"))
+  , (King,       ("King",        "Queen"))
+  ]
 
 data Gender = Male | Female
   deriving (Eq, Show, Enum, Bounded)
@@ -36,4 +56,10 @@ instance Describable Character where
   describe char = "a level " ++ (show $ charLevel char)
                        ++ " " ++ (show $ charGender char)
                        ++ " " ++ (show $ charRace char)
-                       ++ " " ++ (show $ charRole char)
+                       ++ " " ++ role
+    where
+      role = case Data.List.lookup (charRole char) genderedRoles of
+        Nothing                             -> "Nobody"
+        Just (maleRoleName, femaleRoleName) -> if (charGender char == Male )
+                                               then maleRoleName
+                                               else femaleRoleName
