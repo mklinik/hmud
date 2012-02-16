d83d2901f982f7d2c55b29f22924368742383cd8
Markus Klinik <markus.klinik@gmx.de>
2012-02-16 23:17:19 +0100

    Xmpp: first shot for group chat player management

--- Xmpp/Main.hs
+++ Xmpp/Main.hs
@@ -4,6 +4,8 @@ import Network
 import Network.XMPP
 import Network.XMPP.MUC
 import Data.List (isPrefixOf, intercalate)
+import qualified Data.Map as Map
+import Data.Map (Map)
 
 import Hmud.Item
 import Hmud.Describable
@@ -18,8 +20,9 @@ import Hmud.Commands
 botUsername = "markus.klinik"
 botServer = "localhost"
 botPassword = "abc"
-botResource = homepageURL
+botResource = "oracle"
 homepageURL = "https://github.com/mklinik/hmud"
+groupchatJID = "gtf@conference.localhost"
 
 stepToStdout = stepWorld putStrLn
 
@@ -36,7 +39,7 @@ main = withSocketsDo $ do
   handleVersion "hmud" "0.1" "Linux"
   -- ...and do something.
 
-  joinGroupchat "oracle" "gtf@conference.localhost" Nothing
+  joinGroupchat "oracle" groupchatJID Nothing
 
   player <- liftIO $ randomCharacter "Markus"
   npc1 <- liftIO $ randomCharacter "Martin"
@@ -51,16 +54,70 @@ main = withSocketsDo $ do
   w7 <- liftIO $ stepToStdout w6 (insertItem beer "The Black Unicorn")
   w8 <- liftIO $ stepToStdout w7 (insertItem scroll1 "The Black Unicorn")
 
-  run "Markus" w8
+  run (Map.empty, Map.empty) w8
 
-run :: String -> World -> XMPP ()
-run playerName world = do
+type Nick2JidMap = Map String String
+type Jid2PlayerMap = Map String String
+type UserNameMap = (Nick2JidMap, Jid2PlayerMap)
+
+-- TODO "markus.klinik@jabber.ccc.de/foobar" -> "Markus Klinik"
+-- for now: "markus.klinik@jabber.ccc.de/foobar" -> "markus.klinik"
+jid2player :: String -> String
+jid2player jid = if "gtf@conference" `isPrefixOf` jid
+  then (getResource jid)
+  else (getUsername jid)
+
+updateNick :: String -> String -> UserNameMap -> UserNameMap
+updateNick nick jid (nicks, users) = (Map.insert nick jid $ Map.filter ((==) jid) nicks, users)
+
+-- returns (Just newPlayerName, UserNameMap) when there was no such player
+updatePlayerName :: String -> UserNameMap -> (Bool, UserNameMap)
+updatePlayerName jid (nicks, users) = (isNewUser, (nicks, newUsers))
+  where isNewUser = Map.notMember jid users
+        newUsers = Map.insert jid (jid2player jid) users
+
+getPlayerFromNick :: String -> UserNameMap -> Maybe String
+getPlayerFromNick nick (nicks, users) = do
+  jid <- Map.lookup nick nicks
+  Map.lookup jid users
+
+getPlayerFromJid :: String -> UserNameMap -> Maybe String
+getPlayerFromJid jid (_, users) = Map.lookup jid users
+
+run :: UserNameMap -> World -> XMPP ()
+run userNames@(nicks, users) world = do
   -- Wait for an incoming message...
-  msg <- waitForStanza (isChat `conj` hasBody)
-  let sender = maybe "" id (getAttr "from" msg)
-      tokens = words $ maybe "" id (getMessageBody msg)
-  case dispatch playerName tokens of
-    Nothing -> run playerName world
-    Just a  -> do
-      w2 <- stepWorld (sendMessage sender) world a
-      run playerName w2
+  msg <- waitForStanza (const True)
+  if (isChat `conj` hasBody) msg then do
+    let sender = maybe "" id (getAttr "from" msg)
+        tokens = words $ maybe "" id (getMessageBody msg)
+    liftIO $ putStrLn $ "got message from " ++ sender
+    case dispatch (jid2player sender) tokens of
+      Nothing -> run userNames world
+      Just a  -> do
+        w2 <- stepWorld (sendMessage sender) world a
+        run userNames w2
+  else if isGroupchatPresence msg then do
+    -- * maintain nick -> jid mapping
+    -- * maintain jid -> player name mapping
+    -- * greet users as they join (any RoleChange must do for us)
+    liftIO $ putStrLn $ "got groupchat presence from: " ++ (maybe "" id (getAttr "from" msg))
+    let (presence, occupant) = doGroupchatPresence msg
+    (newUserNames, newWorld) <- case presence of
+      RoleChange _ -> -- a user has joined
+        case occJid occupant of
+          Nothing -> return (userNames, world) -- anonymous users don't get a character
+          Just jid ->
+            let (isNewUser, newUserNames) = updatePlayerName jid $ updateNick (occNick occupant) jid userNames
+              in if isNewUser
+                  then do
+                    player <- liftIO $ randomCharacter (jid2player jid)
+                    newWorld <- liftIO $ stepWorld (putStrLn) world (insert player "The Black Unicorn")
+                    sendGroupchatMessage groupchatJID ("Welcome " ++ (name player) ++ ", you are a " ++ (describe player))
+                    return (newUserNames, newWorld)
+                  else return (newUserNames, world)
+      -- NickChange nick -> liftIO $ putStrLn $ "NickChange: " ++ nick -- TODO: track nick changes
+      otherwise -> return (userNames, world)
+    run newUserNames newWorld
+  else do
+    run userNames world
