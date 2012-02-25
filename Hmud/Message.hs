451c1b2987943c2edf4e1729c2dd3e5fdee360c9
Markus Klinik <markus.klinik@gmx.de>
2012-02-25 20:37:45 +0100

    article magic: add 'a' to item names in messages

--- Hmud/Message.hs
+++ Hmud/Message.hs
@@ -1,5 +1,7 @@
 module Hmud.Message where
 
+import Data.Char (isLower)
+
 import Hmud.Character
 import Hmud.Room
 import Hmud.Item
@@ -49,17 +51,20 @@ describeMessage receiver (MsgGoto fromRoom char toRoom)
   | roomHasCharacterExactly receiver toRoom   = (name char) ++ " enters."
   | otherwise = (name char) ++ " goes to " ++ (name toRoom) -- should never happen
 describeMessage receiver (MsgTake char item)
-  | receiver == (charAddress char) = "You take " ++ (name item)
-  | otherwise = (name char) ++ " takes " ++ (name item)
+  | receiver == (charAddress char) = "You take " ++ (doArticleMagic $ name item)
+  | otherwise = (name char) ++ " takes " ++ (doArticleMagic $ name item)
 describeMessage receiver (MsgForge char item)
-  | receiver == (charAddress char) = "The world around you gets dark. All sounds seem to fade. A moment of complete darkness is followed by a bright flash. As you slowly open your eyes again, " ++ (name item) ++ " hovers in the air before you, then floats slowly into your hands."
-  | otherwise = (name char) ++ " forges " ++ (name item)
+  | receiver == (charAddress char) = "The world around you gets dark. All sounds seem to fade. A moment of complete darkness is followed by a bright flash. As you slowly open your eyes again, " ++ (doArticleMagic $ name item) ++ " hovers in the air before you, then floats slowly into your hands."
+  | otherwise = (name char) ++ " forges " ++ (doArticleMagic $ name item)
 describeMessage receiver (MsgPut char item)
-  | receiver == (charAddress char) = "You put " ++ (name item) ++ " down."
-  | otherwise = (name char) ++ " puts " ++ (name item) ++ " down."
+  | receiver == (charAddress char) = "You put " ++ (doArticleMagic $ name item) ++ " down."
+  | otherwise = (name char) ++ " puts " ++ (doArticleMagic $ name item) ++ " down."
 describeMessage receiver (MsgGive giver item givee)
-  | receiver == (charAddress giver) = "You give " ++ (name item) ++ " to " ++ (name givee)
-  | receiver == (charAddress givee) = (name giver) ++ " gives you " ++ (name item)
-  | otherwise = (name giver) ++ " gives " ++ (name item) ++ " to " ++ (name givee)
+  | receiver == (charAddress giver) = "You give " ++ (doArticleMagic $ name item) ++ " to " ++ (name givee)
+  | receiver == (charAddress givee) = (name giver) ++ " gives you " ++ (doArticleMagic $ name item)
+  | otherwise = (name giver) ++ " gives " ++ (doArticleMagic $ name item) ++ " to " ++ (name givee)
 
-
+-- if /name/ starts with a lower letter, add the article 'a' to it
+doArticleMagic :: String -> String
+doArticleMagic [] = []
+doArticleMagic name@(c:cs) = if isLower c then "a " ++ name else name
