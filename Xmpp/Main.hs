8b4e94ca7a9be3b6b2540d29f66e36fbd9bb84f3
Simon Hengel <sol@typeful.net>
2012-02-29 11:06:08 +0100

    Fix layout issues
    This did not compile with ghc-7.0.4.

--- Xmpp/Main.hs
+++ Xmpp/Main.hs
@@ -56,18 +56,20 @@ main = withSocketsDo $ do
   XMPP.getStreamStart c
 
   XMPP.runXMPP c $ do
-  -- ...authenticate...
-  success <- XMPP.startAuth botUsername botServer botPassword botResource
-  if success /= 0 then error "Authentication not successful."
-  else do
-  XMPP.sendPresence (Just ("", [homepageURL, ""])) Nothing
-  XMPP.handleVersion "hmud" "0.1" "Linux"
-  -- ...and do something.
+    -- ...authenticate...
+    success <- XMPP.startAuth botUsername botServer botPassword botResource
+    if success /= 0
+      then
+        error "Authentication not successful."
+      else do
+        XMPP.sendPresence (Just ("", [homepageURL, ""])) Nothing
+        XMPP.handleVersion "hmud" "0.1" "Linux"
+        -- ...and do something.
 
-  XMPP.joinGroupchat "oracle" groupchatJID groupchatPassword
+        XMPP.joinGroupchat "oracle" groupchatJID groupchatPassword
 
-  run world
-  return ()
+        run world
+        return ()
 
 instance MonadHmud XMPP where
   waitForMessage = waitForMessageXmpp
@@ -81,37 +83,41 @@ instance MonadHmud XMPP where
 waitForMessageXmpp :: XMPP IncomingMessage
 waitForMessageXmpp = do
   stanza <- XMPP.waitForStanza (const True)
-  if (XMPP.isChat `XMPP.conj` XMPP.hasBody) stanza then do
-    let sender = XMPP.getAttr "from" stanza
-        tokens = words $ maybe "" id (XMPP.getMessageBody stanza)
-    case sender of
-      Nothing -> waitForMessageXmpp
-      Just playerAddr | playerAddr == botJID -> waitForMessageXmpp
-      Just playerAddr -> do
-        XMPP.liftIO $ logString $ ">> " ++ playerAddr ++ ": " ++ (unwords tokens)
-        return $ MsgCommand (Just playerAddr) tokens
-  else if XMPP.isGroupchatPresence stanza then do
-    let sender = XMPP.getAttr "from" stanza
-    case sender of
-      Nothing -> waitForMessageXmpp
-      playerAddr -> do
-        let (presence, occupant) = XMPP.doGroupchatPresence stanza
-        case presence of
-          XMPP.RoleChange _ -> do -- a user has joined or changed nick
-            case XMPP.occJid occupant of
-              Nothing -> waitForMessageXmpp -- anonymous users don't get a character
-              Just jid -> if (jid == botJID)
-                then waitForMessageXmpp -- filter presence msg from myself
-                else do
-                  let msg = MsgPlayerEnters playerAddr (jid2player jid) (jid2primKey jid)
-                  XMPP.liftIO $ logString $ ">> " ++ show msg
-                  return msg
-          otherwise -> waitForMessageXmpp
-  else if XMPP.isIq stanza && isJust (XMPP.xmlPath ["ping"] stanza) then do
-    let idAttr = maybe "" id (XMPP.getAttr "id" stanza)
-        from = maybe "" id (XMPP.getAttr "from" stanza)
-        to =  maybe "" id (XMPP.getAttr "to" stanza)
-        pong = XMPP.XML "iq" [("from", to), ("to", from), ("id", idAttr), ("type", "result")] []
-    XMPP.sendStanza pong
-    waitForMessageXmpp
-  else waitForMessageXmpp
+  if (XMPP.isChat `XMPP.conj` XMPP.hasBody) stanza
+    then do
+      let sender = XMPP.getAttr "from" stanza
+          tokens = words $ maybe "" id (XMPP.getMessageBody stanza)
+      case sender of
+        Nothing -> waitForMessageXmpp
+        Just playerAddr | playerAddr == botJID -> waitForMessageXmpp
+        Just playerAddr -> do
+          XMPP.liftIO $ logString $ ">> " ++ playerAddr ++ ": " ++ (unwords tokens)
+          return $ MsgCommand (Just playerAddr) tokens
+    else if XMPP.isGroupchatPresence stanza
+      then do
+        let sender = XMPP.getAttr "from" stanza
+        case sender of
+          Nothing -> waitForMessageXmpp
+          playerAddr -> do
+            let (presence, occupant) = XMPP.doGroupchatPresence stanza
+            case presence of
+              XMPP.RoleChange _ -> do -- a user has joined or changed nick
+                case XMPP.occJid occupant of
+                  Nothing -> waitForMessageXmpp -- anonymous users don't get a character
+                  Just jid -> if (jid == botJID)
+                    then waitForMessageXmpp -- filter presence msg from myself
+                    else do
+                      let msg = MsgPlayerEnters playerAddr (jid2player jid) (jid2primKey jid)
+                      XMPP.liftIO $ logString $ ">> " ++ show msg
+                      return msg
+              otherwise -> waitForMessageXmpp
+      else if XMPP.isIq stanza && isJust (XMPP.xmlPath ["ping"] stanza)
+        then do
+          let idAttr = maybe "" id (XMPP.getAttr "id" stanza)
+              from = maybe "" id (XMPP.getAttr "from" stanza)
+              to =  maybe "" id (XMPP.getAttr "to" stanza)
+              pong = XMPP.XML "iq" [("from", to), ("to", from), ("id", idAttr), ("type", "result")] []
+          XMPP.sendStanza pong
+          waitForMessageXmpp
+        else
+          waitForMessageXmpp
