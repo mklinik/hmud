fef20a957512104582e80c6206250f92e241a64b
Markus Klinik <markus.klinik@gmx.de>
2012-03-02 23:39:22 +0100

    RealData: fix compile warnings

--- Hmud/RealData.hs
+++ Hmud/RealData.hs
@@ -2,29 +2,32 @@ module Hmud.RealData where
 
 import Hmud.Item
 import Hmud.Describable
-import Hmud.Character
 import Hmud.Room
 import Hmud.World
-import Hmud.Util
 
+theScrollOfForgery :: Item
 theScrollOfForgery = Item { itemName = "scroll of forgery", itemDescription = "enables the forge command" }
 
+tavern :: Room
 tavern = mkRoom
   "Black Unicorn"
   "a dusty dark tavern. It smells of delicious food. You hear cheery background music."
   [(name townSquare)]
   [theScrollOfForgery]
 
+townSquare :: Room
 townSquare = mkRoom
   "town square"
   "the central meeting place of the town. There is a fountain, trees and flowers, and lots of people that are busy with their daily routine. The sun shines, birds sing and everybody is quite happy."
   [(name tavern), (name ivoryTower)]
   []
 
+ivoryTower :: Room
 ivoryTower = mkRoom
   "ivory tower"
   "a tall white building with long hallways, large laboratories and a big library. Inside it is completely quiet, except for the occasional reverberant sound of footsteps. In this place, scholars develop new crazy magic spells. You are at the very top of the tower, in a small chamber with windows to all sides. You can see the whole town from up here."
   [(name townSquare)]
   [theScrollOfForgery]
 
+world :: World
 world = World { worldRooms = [tavern, townSquare, ivoryTower], idleCharacters = [] }
